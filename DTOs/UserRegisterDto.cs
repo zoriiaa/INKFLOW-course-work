8ed599433683796d7a586b7c607877ca6806a4ac
Petrivska Zoreslava <petrivska.zzoreslava@gmail.com>
2026-05-23 20:56:40 +0300

    Add validation attributes to UserRegisterDto for email and password fields

--- DTOs/UserRegisterDto.cs
+++ DTOs/UserRegisterDto.cs
@@ -1,8 +1,12 @@
 ﻿namespace INKFLOW.DTOs;
+using System.ComponentModel.DataAnnotations;
 
 public class UserRegisterDto
 {
     public string Username { get; set; } = string.Empty;
+    [Required(AllowEmptyStrings = false)]
+    [EmailAddress]
     public string Email { get; set; } = string.Empty;
+    [Required]
     public string Password { get; set; } = string.Empty;
 }
